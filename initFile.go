a052997b20a1cd82e26bfb775109e647f1f4c076
TimeForANinja <TimeForANinja@users.noreply.github.com>
2019-07-11 01:23:06 +0200

    Update initFile.go
    * cleanup
    * add database#passphrase
    * change a lot of privacy settings
    * add int16/32 <-> []byte conversion functions
    * major progress on loading/creating/parsing the first 128 bytes of a database file
    * simplify how the "NewDBEnc" function generates the first 128 bytes

--- initFile.go
+++ initFile.go
@@ -12,11 +12,24 @@ import (
 	"os"
 )
 
-type Database struct {
-	fileinfo      fileInfo
-	file          *os.File
-	encryption    bool
-	encryptionKey []byte
+type version struct {
+	major int8
+	minor int8
+	patch int8
+}
+
+/*
+ * everything about a page in the database
+ */
+type tablePage struct {
+	indexOfTable            int64
+	numberOfColumns         int64
+	indexOfColumnDefPage    int64
+	indexOfFirstContentPage int64
+	pageCount               int64
+	rowCount                int64
+	tableNameLength         int64
+	tableName               string
 }
 
 func readPage(db *Database) *tablePage {
@@ -42,34 +55,120 @@ func parsePage([]byte) *tablePage {
 	return nil
 }
 
-type Version struct {
-	Major int8
-	Minor int8
-	Patch int8
+/*
+ * data transformation utilities
+ */
+func int16toBytes(int int16) []byte {
+	return []byte{
+		byte(int >> 8),
+		byte(int & 255),
+	}
+}
+func int32toBytes(int int32) []byte {
+	return []byte{
+		byte((int >> 24) & 255),
+		byte((int >> 16) & 255),
+		byte((int >> 8) & 255),
+		byte(int & 255),
+	}
+}
+func bytesToInt16(bytes []byte) int16 {
+	return (int16(bytes[0]) << 8) + int16(bytes[1])
+}
+func bytesToInt32(bytes []byte) int32 {
+	return (int32(bytes[2]) << 24) + (int32(bytes[2]) << 16) + (int32(bytes[2]) << 8) + int32(bytes[3])
 }
 
+/*
+ * utility for the meta information in the first 128 bytes
+ */
 type fileInfo struct {
-	Version              Version
-	databasePageSize     int32
-	fileChangeCounter    int64
-	databasePages        int64
-	defaultPageCacheSize int64
+	version              version
+	databasePageSize     int16
+	fileChangeCounter    int32
+	databasePages        int32
+	defaultPageCacheSize int32
 }
 
-func newFileInfo(data []byte) fileInfo {
-	// TODO: parse fileinfo in here
-	return fileInfo{}
+func (f fileInfo) toBytes() []byte {
+	resp := make([]byte, 17)
+	resp[0] = byte(f.version.major)
+	resp[1] = byte(f.version.minor)
+	resp[2] = byte(f.version.patch)
+	copy(resp[3:5], int16toBytes(f.databasePageSize))
+	copy(resp[5:9], int32toBytes(f.fileChangeCounter))
+	copy(resp[9:13], int32toBytes(f.databasePages))
+	copy(resp[13:17], int32toBytes(f.defaultPageCacheSize))
+	return resp
 }
 
-type tablePage struct {
-	indexOfTable            int64
-	numberOfColumns         int64
-	indexOfColumnDefPage    int64
-	indexOfFirstContentPage int64
-	pageCount               int64
-	rowCount                int64
-	tableNameLength         int64
-	tableName               string
+func parseFileInfo(data []byte) fileInfo {
+	f := fileInfo{}
+	f.version = version{
+		major: int8(data[0]),
+		minor: int8(data[1]),
+		patch: int8(data[2]),
+	}
+	f.databasePageSize = bytesToInt16(data[3:5])
+	f.fileChangeCounter = bytesToInt32(data[5:9])
+	f.databasePages = bytesToInt32(data[9:13])
+	f.defaultPageCacheSize = bytesToInt32(data[13:17])
+	return f
+}
+func defaultFileInfo() fileInfo {
+	return fileInfo{
+		version:              version{major: 0, minor: 0, patch: 1},
+		databasePageSize:     4096,
+		fileChangeCounter:    0,
+		databasePages:        0,
+		defaultPageCacheSize: 0,
+	}
+}
+
+/*
+ * main Database object & exported functions
+ */
+type Database struct {
+	fileinfo      fileInfo
+	file          *os.File
+	passphrase    string
+	encryption    bool
+	encryptionKey []byte
+}
+
+func (db Database) writeHead() error {
+	data := make([]byte, 128)
+	if db.encryption {
+		iv, err := randomIV(16)
+		if err != nil {
+			return err
+		}
+		copy(data[:16], iv)
+
+		content := make([]byte, 112)
+		copy(content[:9], []byte{103, 111, 68, 66, 32, 101, 110, 99, 00})
+		copy(content[9:41], db.encryptionKey)
+		copy(content[41:], db.fileinfo.toBytes())
+
+		encryptedData, err := encryptCFB(content, iv, hash([]byte(db.passphrase), iv))
+		if err != nil {
+			return err
+		}
+		copy(data[16:], encryptedData)
+	} else {
+		copy(data[:5], []byte{103, 111, 68, 66, 00})
+		copy(data[5:], db.fileinfo.toBytes())
+	}
+	// TODO: check whether data is still 128 byte long
+	fmt.Print("writeHead for ")
+	if !db.encryption {
+		fmt.Print("un")
+	}
+	fmt.Print("encrypted database with length ")
+	fmt.Println(len(data))
+
+	db.file.WriteAt(data, 0)
+	return nil
 }
 
 func Open(file string) (*Database, error) {
@@ -87,11 +186,11 @@ func Open(file string) (*Database, error) {
 	if !bytes.Equal(data[:5], []byte{103, 111, 68, 66, 00}) {
 		return nil, errors.New("Invalid File")
 	}
-	db.fileinfo = newFileInfo(data[5:])
+	db.fileinfo = parseFileInfo(data[5:])
 	return db, nil
 }
 func OpenEnc(file, passphrase string) (*Database, error) {
-	db := &Database{encryption: true}
+	db := &Database{encryption: true, passphrase: passphrase}
 	var err error
 
 	db.file, err = os.OpenFile(file, os.O_RDWR, 0666)
@@ -111,34 +210,32 @@ func OpenEnc(file, passphrase string) (*Database, error) {
 		return nil, errors.New("Invalid File")
 	}
 	db.encryptionKey = dataDec[9:41]
-	db.fileinfo = newFileInfo(dataDec[41:])
+	db.fileinfo = parseFileInfo(dataDec[41:])
 	return db, nil
 }
 func NewDBEnc(file, passphrase string) (*Database, error) {
-	db := &Database{encryption: true}
+	db := &Database{encryption: true, passphrase: passphrase}
 	var err error
 
 	db.file, err = os.Create(file)
 	if err != nil {
 		return nil, err
 	}
-	dataDec := make([]byte, 112)
-	copy(dataDec[:9], []byte{103, 111, 68, 66, 32, 101, 110, 99, 00})
 	db.encryptionKey, err = randomIV(32)
 	if err != nil {
 		return nil, err
 	}
-	copy(dataDec[9:41], db.encryptionKey)
-	dataDec[41] = 69
-	cipher, iv, err := newEncryptCFB(dataDec, passphrase)
+	db.fileinfo = defaultFileInfo()
+	err = db.writeHead()
 	if err != nil {
 		return nil, err
 	}
-	db.file.Write(append(iv[:], cipher[:]...))
-	db.fileinfo = newFileInfo(dataDec[41:])
 	return db, nil
 }
 
+/*
+ * bullshit functions
+ */
 func main() {
 	_, err := NewDBEnc("./test.db.enc", "asdfMovie")
 	fmt.Println("-- newDBEnc returned")
@@ -150,7 +247,9 @@ func main() {
 	fmt.Println(err2)
 }
 
-// cryptography methods
+/*
+ * cryptography functions
+ */
 func hash(data, salt []byte) []byte {
 	h := sha256.New()
 	h.Write(data)
