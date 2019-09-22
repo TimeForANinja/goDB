9840501535b7a645b18c70f062ef19fb8b07b0ae
TimeForANinja <t.kutscha@yahoo.de>
2019-09-22 22:15:24 +0200

    use default methods in dataTransform.go

--- dataTransform.go
+++ dataTransform.go
@@ -1,25 +1,41 @@
 package main
 
+import "encoding/binary"
+
 /*
  * data transformation utilities
  */
-func int16toBytes(int int16) []byte {
-	return []byte{
-		byte(int >> 8),
-		byte(int & 255),
-	}
+func uint16toBytes(num uint16) []byte {
+	b := make([]byte, 2)
+	binary.BigEndian.PutUint16(b, num)
+	return b
+	//	return []byte{
+	//		byte(uint >> 8),
+	//		byte(uint & 255),
+	//	}
 }
-func int32toBytes(int int32) []byte {
-	return []byte{
-		byte((int >> 24) & 255),
-		byte((int >> 16) & 255),
-		byte((int >> 8) & 255),
-		byte(int & 255),
-	}
+func uint32toBytes(num uint32) []byte {
+	b := make([]byte, 4)
+	binary.BigEndian.PutUint32(b, num)
+	return b
+	//	return []byte{
+	//		byte((int >> 24) & 255),
+	//		byte((int >> 16) & 255),
+	//		byte((int >> 8) & 255),
+	//		byte(int & 255),
+	//	}
 }
-func bytesToInt16(bytes []byte) int16 {
-	return (int16(bytes[0]) << 8) + int16(bytes[1])
+func stringtoBytes(s string) []byte {
+	return []byte(s)
 }
-func bytesToInt32(bytes []byte) int32 {
-	return (int32(bytes[2]) << 24) + (int32(bytes[2]) << 16) + (int32(bytes[2]) << 8) + int32(bytes[3])
+func bytesToUInt16(bytes []byte) uint16 {
+	return binary.BigEndian.Uint16(bytes)
+	// return (uint16(bytes[0]) << 8) + uint16(bytes[1])
+}
+func bytesToUInt32(bytes []byte) uint32 {
+	return binary.BigEndian.Uint32(bytes)
+	// return (uint32(bytes[2]) << 24) + (uint32(bytes[2]) << 16) + (uint32(bytes[2]) << 8) + uint32(bytes[3])
+}
+func bytesToString(bytes []byte) string {
+	return string(bytes)
 }
