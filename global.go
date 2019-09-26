4612b6ed53d61415d76d62045eb2a4c83d0d1664
TimeForANinja <t.kutscha@yahoo.de>
2019-09-26 22:36:32 +0200

    add global variables

---
+++ global.go
@@ -0,0 +1,12 @@
+package goDB
+
+// MAJOR part of Version number
+var MAJOR uint8 = 1
+
+// MINOR part of Version number
+var MINOR uint8 = 0
+
+// PATCH part of Version number
+var PATCH uint8 = 0
+
+var DEFAULT_PAGE_SIZE uint16 = 4096
