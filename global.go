0d1c0a34c0998df3a7694dcd19c039a879c2755f
TimeForANinja <t.kutscha@yahoo.de>
2019-09-29 12:54:58 +0200

    make global vars const

--- global.go
+++ global.go
@@ -1,12 +1,13 @@
 package goDB
 
 // MAJOR part of Version number
-var MAJOR uint8 = 1
+const MAJOR uint8 = 1
 
 // MINOR part of Version number
-var MINOR uint8 = 0
+const MINOR uint8 = 00
 
 // PATCH part of Version number
-var PATCH uint8 = 0
+const PATCH uint8 = 00
 
-var DEFAULT_PAGE_SIZE uint16 = 4096
+// DEFAULT_PAGE_SIZE (= 4096)
+const DEFAULT_PAGE_SIZE uint16 = 4096
