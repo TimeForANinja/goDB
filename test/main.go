811b79289a268d05d530b3f59123b6e40faf9443
TimeForANinja <t.kutscha@yahoo.de>
2019-09-29 12:55:16 +0200

    expand on test/main.go

--- test/main.go
+++ test/main.go
@@ -7,12 +7,30 @@ import (
 )
 
 func main() {
+	newDB()
+	newEncDB()
+}
+
+func newDB() {
 	db := goDB.NewDB()
 	err := db.Open("test.db")
 	if err != nil {
 		fmt.Print("Failed: ")
 		fmt.Println(err)
 	} else {
-		fmt.Println("Success!")
+		fmt.Print("Success!")
+		fmt.Println(db)
+	}
+}
+
+func newEncDB() {
+	db := goDB.NewEncDB("myPW")
+	err := db.Open("test.enc.db")
+	if err != nil {
+		fmt.Print("Failed: ")
+		fmt.Println(err)
+	} else {
+		fmt.Print("Success!")
+		fmt.Println(db)
 	}
 }
