8227319a6783918ace545eefdfbd4a00bfa426dd
TimeForANinja <t.kutscha@yahoo.de>
2019-10-23 12:47:14 +0200

    add to PageStruct_test.go

--- PageStruct_test.go
+++ PageStruct_test.go
@@ -2,6 +2,8 @@ package goDB
 
 import (
 	"testing"
+
+	util "github.com/timeforaninja/goDB/utility"
 )
 
 func TestPage(t *testing.T) {
@@ -35,6 +37,53 @@ func TestPage(t *testing.T) {
 	}
 	// deserialize again
 	decodedHead, decodedBytes, err := deserializePage(testPageSerial, false, nil)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	testPageDecoded := page{
+		index:    2,
+		pageHead: decodedHead,
+		data:     decodedBytes,
+	}
+	// compare
+	if !myPage.equals(&testPageDecoded) {
+		t.Error("objects not equal", myPage, testPageDecoded)
+	}
+}
+
+func TestEncPage(t *testing.T) {
+	testKey, _ := util.RandomIV(32)
+	myPageSize := 128
+	myData := make([]byte, 64)
+	copy(myData, []byte{255, 254, 253, 252, 251, 250, 249, 248, 247, 246, 245, 244, 243, 242, 241, 240, 0, 0, 0, 0})
+	myPageSpezific := make([]byte, 35)
+	copy(myPageSpezific, []byte{69})
+	myPageHead := pageHead{
+		pageType:     1,
+		nextPage:     1337,
+		prevPage:     420,
+		firstItem:    10,
+		endTrim:      69,
+		pageSpezific: myPageSpezific,
+	}
+	myPage := page{
+		index:    2,
+		pageHead: &myPageHead,
+		data:     myData,
+	}
+
+	// serialize page
+	testPageSerial, err := myPage.serializePage(uint16(myPageSize), true, testKey)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if len(testPageSerial) != myPageSize {
+		t.Error("testPageSerial invalid length", testPageSerial, myPageSize)
+	}
+	// deserialize again
+	decodedHead, decodedBytes, err := deserializePage(testPageSerial, true, testKey)
 	if err != nil {
 		t.Error(err)
 		return
