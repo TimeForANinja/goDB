68ab34523b52c1a17f3591b8129508cd3466f7d6
TimeForANinja <t.kutscha@yahoo.de>
2019-11-06 14:58:25 +0100

    keep working on the iterator

--- EmptyTableListStruct.go
+++ EmptyTableListStruct.go
@@ -10,16 +10,44 @@ type emptyTableListIterator struct {
 	currPage *page
 }
 
-func (etli *emptyTableListIterator) next() uint32 {
-	if(etli.position.Byte == etli.db.head.pageSize) // do sth special
-	etli.position.Byte += 4
-	return util.BytesToUInt32(etli.currPage.data[etli.position.Byte-4 : 4])
+func (etli *emptyTableListIterator) forward() error {
+	if (etli.position.Byte - etli.currPage.pageHead.endTrim) == etli.db.head.pageSize {
+		var err error
+		etli.position.Page = etli.currPage.pageHead.nextPage
+		etli.currPage, err = ReadPage(etli.db, etli.currPage.pageHead.nextPage)
+		if err != nil {
+			return err
+		}
+		etli.position.Byte = etli.currPage.pageHead.firstItem
+	} else {
+		etli.position.Byte += 4
+	}
+	return nil
 }
 
-func (db *database) getEmptyTableListIterator() *emptyTableListIterator {
-	return emptyTableListIterator{
-		db:       db,
-		position: Position{db.head.emptyPagesListLocation},
-		currPage: ReadPage(db, db.head.emptyPagesListLocation),
+func (etli *emptyTableListIterator) read() uint32 {
+	return util.BytesToUInt32(etli.currPage.data[etli.position.Byte:4])
+}
+
+func (etli *emptyTableListIterator) hasNext() bool {
+	if (etli.position.Byte - etli.currPage.pageHead.endTrim) == etli.db.head.pageSize {
+		return etli.currPage.pageHead.nextPage != 0
 	}
+	return true
+}
+
+func (db *database) getEmptyTableListIterator() (*emptyTableListIterator, error) {
+	page, err := ReadPage(db, db.head.emptyPagesListLocation)
+	if err != nil {
+		return nil, err
+	}
+	pos := util.Position{
+		Page: db.head.emptyPagesListLocation,
+		Byte: page.pageHead.firstItem,
+	}
+	return &emptyTableListIterator{
+		db:       db,
+		position: &pos,
+		currPage: page,
+	}, nil
 }
