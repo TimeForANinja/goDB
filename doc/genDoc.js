0efe70d59b19c6d6bc6e0676154d5e9605d54fcd
TimeForANinja <t.kutscha@yahoo.de>
2019-11-22 12:52:18 +0100

    update genDoc.js

--- doc/genDoc.js
+++ doc/genDoc.js
@@ -9,16 +9,26 @@ const IGNORE = [
   '.git'
 ];
 
+const main = async () => {
+  const doc = await parseDir(PATH.resolve(__dirname, '../'));
+  console.log(doc);
+}
+setImmediate(main);
+
 const parseDir = async (rootDir, workDir = "") => {
   const tail = await checkForOtherDir(rootDir, workDir);
 
   const stdout = await execPromise('go doc -u -all', PATH.resolve(rootDir, workDir));
   const parts = stdout.trim().split(/\n\n\n(?=[A-Z]+\n)/);
 
-  const package = PARTS.find(a => a.startsWith('package')) || null;
-  const constants = await parseConstants(parts.find(a => a.startsWith('CONSTANTS')), package, rootDir, workDir);
-  const functions = await parseFunctions(parts.find(a => a.startsWith('FUNCTIONS')), package, rootDir, workDir);
-  const types = await parseTypes(parts.find(a => a.startsWith('TYPES')), package, rootDir, workDir);
+  const package = parsePackage(parts.findPop(a => a.startsWith('package')));
+  const constants = await parseConstants(parts.findPop(a => a.startsWith('CONSTANTS')), package, rootDir, workDir);
+  const functions = await parseFunctions(parts.findPop(a => a.startsWith('FUNCTIONS')), package, rootDir, workDir);
+  const types = await parseTypes(parts.findPop(a => a.startsWith('TYPES')), package, rootDir, workDir);
+  if(parts.length) {
+    FS.writeFileSync(PATH.resolve(__dirname, './unknown.json'), JSON.stringify(parts));
+    throw Error(`unknown types in "${PATH.resolve(rootDir, workDir)}"\nplease share "${PATH.resolve(__dirname, './unknown.json')}" to help improve this package`);
+  }
   tail.push(
     ...constants,
     ...functions,
@@ -26,7 +36,13 @@ const parseDir = async (rootDir, workDir = "") => {
   );
   return tail;
 }
-setImmediate(parseDir, PATH.resolve(__dirname, '../'));
+
+Array.prototype.findPop = function(callback) {
+  const obj = this.find(callback);
+  if(obj === undefined) return null;
+  this.splice(this.indexOf(obj) , 1);
+  return obj;
+}
 
 const checkForOtherDir = async (rootDir, workDir) => {
   const tail = [];
@@ -48,28 +64,33 @@ const checkForOtherDir = async (rootDir, workDir) => {
   return tail;
 }
 
+const parsePackage = (content) => {
+  if(!content) return null;
+  return content.match(/package ([a-zA-Z0-9_]+)/)[1];
+}
 const parseConstants = async (content, package, rootDir, workDir) => {
   if(!content) return [];
   const constants = content.split('\n\n').slice(1);
-  return constants.map(async a => {
+  const parsedConstants = constants.map(async a => {
     const delimiter = a.indexOf('\n');
 
     const code = a.substr(0, delimiter);
     const definition = code.match(/(const\s)?([a-zA-Z0-9_]+)(\s[a-zA-Z0-9]+)? = ([\s\S]+)/);
-    const { file, line } = await findFileAndLine(rootDir, workDir, code)
+    const { file, line } = await findFileAndLine(rootDir, workDir, code);
 
     return {
       package,
       file: PATH.relative(rootDir, PATH.resolve(rootDir, workDir, file)),
       line,
       code,
-      exported: definition[1].test(/^[A-Z]/),
+      exported: (/^[A-Z]/).test(definition[1]),
       name: nullsaveTrim(definition[1]),
       type: nullsaveTrim(definition[2]),
       value: nullsaveTrim(definition[3]),
       description: nullsaveTrim(a.substr(delimiter)),
     }
   });
+  return await Promise.all(parsedConstants);
 }
 const parseFunctions = async (content, rootDir, workDir) => {
   return [];
